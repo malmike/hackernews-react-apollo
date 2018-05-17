75795c54cafb9375a7659231dfade8cf3fad9554
Michael Male <michael.male@andela.com>
2018-05-17 11:54:22 +0300

    Add routes to the application

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../styles/App.css';
 
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
+import Header from './Header.js';
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return(
+      <div className="center w85">
+      <Header />
+        <div className="ph3 pv1 background-grey">
+          <Switch>
+            <Route exact path="/" component={ LinkList } />
+            <Route exact path="/create" component={ CreateLink } />
+          </Switch>
+        </div>
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>
+    , document.getElementById('root')
+);
+
 registerServiceWorker();
